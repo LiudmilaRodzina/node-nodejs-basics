fd2edb9cc102b288ac169efbcda32ba0cfc39c8d
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-10-05 15:57:44 +0300

    feat: complete fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here
+  const folder = './src/fs/files';
+  const folderCopy = './src/fs/files_copy';
+
+  try {
+    await fs.access(folder);
+
+    try {
+      await fs.access(folderCopy);
+      throw new Error('FS operation failed');
+    } catch {
+      await fs.mkdir(folderCopy);
+    }
+
+    const files = await fs.readdir(folder);
+    for (const file of files) {
+      await fs.copyFile(`${folder}/${file}`, `${folderCopy}/${file}`);
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
