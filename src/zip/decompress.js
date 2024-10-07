1caa089938bdc0b72b0b48cf9eec2833bde53f79
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-10-08 02:18:54 +0300

    feat: complete zip/decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const inputFile = './src/zip/files/archive.gz';
+  const outputFile = './src/zip/files/fileToCompress.txt';
+
+  const readStream = createReadStream(inputFile);
+  const gunzip = createGunzip();
+  const writeStream = createWriteStream(outputFile);
+
+  readStream.on('data', (chunk) => {
+    gunzip.write(chunk);
+  });
+
+  readStream.on('end', () => {
+    gunzip.end();
+  });
+
+  gunzip.on('data', (chunk) => {
+    writeStream.write(chunk);
+  });
 };
 
 await decompress();
