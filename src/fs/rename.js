81214277adb79f18b85c1dc2376e40907f7a73b2
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-10-07 14:32:29 +0300

    feat: complete fs/rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
 const rename = async () => {
-    // Write your code here 
+  const wrongFile = './src/fs/files/wrongFilename.txt';
+  const properFile = './src/fs/files/properFilename.md';
+
+  try {
+    await fs.access(wrongFile);
+
+    try {
+      await fs.access(properFile);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      await fs.rename(wrongFile, properFile);
+    }
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
