e10618743425b112c547fd5426d044039ef72396
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-10-08 01:27:37 +0300

    feat: complete streams/transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+  process.stdout.write(
+    'Please type the text that needs to be reversed and press Enter\n'
+  );
+
+  const reverseTransform = new Transform({
+    transform(chunk, _, callback) {
+      const reversed = chunk.toString().split('').reverse().join('');
+      callback(null, reversed);
+    },
+  });
+
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
 
 await transform();
