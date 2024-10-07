df708cc6266c9d0bb19c92906b21aa47981aa007
Liudmila Rodzina <liudmilarodzina@gmail.com>
2024-10-07 23:46:59 +0300

    feat: complete hash/calcHash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const file = './src/hash/files/fileToCalculateHashFor.txt';
+  const hash = createHash('sha256');
+
+  const stream = createReadStream(file);
+
+  stream.on('data', (chunk) => hash.update(chunk));
+  stream.on('end', () => console.log(hash.digest('hex')));
 };
 
 await calculateHash();
